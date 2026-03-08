ff025a9b6b776fbfee771d1a8ac41534c6fac84e
Joabe-BackEnd <joabe.4njos@gmail.com>
2026-03-08 08:34:08 -0300

    teste

---
+++ src/test/java/br/com/jobs/CadastroNinja/CadastroNinjaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jobs.CadastroNinja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroNinjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
