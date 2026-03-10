69a3c97ecd96405d90e12f217dd29f68fd6a7c8c
Joabe-BackEnd <joabe.4njos@gmail.com>
2026-03-10 08:45:12 -0300

    Criado a controller

--- src/main/java/br/com/jobs/CadastroNinja/CadastroNinjaApplication.java
+++ src/main/java/br/com/jobs/CadastroNinja/CadastroNinjaApplication.java
@@ -10,4 +10,5 @@ public class CadastroNinjaApplication {
 		SpringApplication.run(CadastroNinjaApplication.class, args);
 	}
 
+
 }
---
+++ src/main/java/br/com/jobs/CadastroNinja/controllers/Controller.java
@@ -0,0 +1,16 @@
+package br.com.jobs.CadastroNinja.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/oirota")
+    public String dizOiPraRotaFilho(){
+        return "OOOI ROTA :)!";
+    }
+
+}
