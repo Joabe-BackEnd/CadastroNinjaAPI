a761564b90732ca91092f27a6fc42b2e646d5eb9
Joabe-BackEnd <joabe.4njos@gmail.com>
2026-03-11 18:47:15 -0300

    feat: Altera a rota do controller para listar os ninjas e injeta a dependencia SERVICE e do REPOSITORY

--- src/main/java/br/com/jobs/CadastroNinja/ninjas/NinjaController.java
+++ src/main/java/br/com/jobs/CadastroNinja/ninjas/NinjaController.java
@@ -1,16 +1,48 @@
 package br.com.jobs.CadastroNinja.ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("/ninjas")
 public class NinjaController {
+
+    private NinjaService ninjaService;
+
+    public NinjaController(NinjaService ninjaService) {
+        this.ninjaService = ninjaService;
+    }
 
     @GetMapping("/oirota")
     public String dizOiPraRotaFilho(){
         return "OOOI ROTA :)!";
+    }
+
+    //adicionar ninja
+    @PostMapping("/criar")
+    public String criarNinja(){
+        return "Ninja adcionado!";
+    }
+    //mostrar todos os ninjas
+    @GetMapping("/todos")
+    public List<NinjaModel> listarTodosOsNinjas(){
+        return ninjaService.listarTodosOsNinjas();
+    }
+    //mostrar ninja por id
+    @GetMapping("/todosID")
+    public String mostrarTodosOsNinjasPorId(){
+        return "Informações mostradas!";
+    }
+    //alterar dados ninja
+    @PutMapping("/alterarID")
+    public String alterarNinjaPorId(){
+        return "Informações alteradas";
+    }
+    //deletar ninja
+    @DeleteMapping("/deletarID")
+    public String deletarNinja(){
+        return "Ninja deletado da tabela";
     }
 
 }
--- src/main/java/br/com/jobs/CadastroNinja/ninjas/NinjaService.java
+++ src/main/java/br/com/jobs/CadastroNinja/ninjas/NinjaService.java
@@ -1,4 +1,21 @@
 package br.com.jobs.CadastroNinja.ninjas;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class NinjaService {
+
+    private NinjaRepository ninjaRepository;
+
+    public NinjaService(NinjaRepository ninjaRepository) {
+        this.ninjaRepository = ninjaRepository;
+    }
+
+    // listar todos os meus ninjas
+    public List<NinjaModel> listarTodosOsNinjas(){
+        return ninjaRepository.findAll();
+    }
+
 }
