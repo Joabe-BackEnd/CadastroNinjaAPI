33c6fee5b40ef1c8568ba85aedbe26b5fb07ab3b
Joabe-BackEnd <joabe.4njos@gmail.com>
2026-03-14 00:18:08 -0300

    chore: Inclui a versao do DTO pela entidade e mapeia manualmente no NinjaMapper os caminhos

--- src/main/java/br/com/jobs/CadastroNinja/ninjas/NinjaDTO.java
+++ src/main/java/br/com/jobs/CadastroNinja/ninjas/NinjaDTO.java
@@ -1,4 +1,21 @@
 package br.com.jobs.CadastroNinja.ninjas;
 
+import br.com.jobs.CadastroNinja.missoes.MissoesModel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+    private String rank;
+    private MissoesModel missao;
+
 }
